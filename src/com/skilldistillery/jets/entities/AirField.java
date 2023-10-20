47c25904e2dbcb603add2dc366b6101a6ac8e07f
Parrisu <parris.a.jones@gmail.com>
2023-10-20 15:35:05 -0700

    initial setup

---
+++ src/com/skilldistillery/jets/entities/AirField.java
@@ -0,0 +1,27 @@
+package com.skilldistillery.jets.entities;
+
+import com.skilldistillery.jets.app.*;
+
+import java.util.List;
+
+public class AirField {
+	private List<Jet> fleet;
+	
+	
+	//?
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
